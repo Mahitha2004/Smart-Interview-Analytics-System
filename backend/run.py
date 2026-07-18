fcee10506bf4fab2643d584a9287fff9df7b1f79
mahitha <mahithadpillai@gmail.com>
2026-07-18 20:58:54 +0530

    Add gunicorn

--- backend/run.py
+++ backend/run.py
@@ -14,5 +14,5 @@ except (ImportError, AttributeError):
 from app import create_app
 app = create_app()
 if __name__ == '__main__':
-    print("Starting backend server safely on http://127.0.0.1:5000 ...")
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
